321f062de1bc49f9f4ddd2821c1a7d17dbaeab46
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-04-12 20:33:57 -0700

    taskbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar/Navbar";
+import Taskbar from "@/components/Taskbar";
 
 
 export default function RootLayout({
@@ -14,6 +15,7 @@ export default function RootLayout({
       <body>
         <div>
           <Navbar />
+          <Taskbar />
         </div>
       </body>
     </html>
---
+++ src/components/Taskbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Taskbar = () => { 
+    return (
+        <div className="fixed bottom-0 left-0 right-0 h-12 
+            bg-gradient-to-t from-[#2961DE] to-[#2157D5] 
+            border-t border-[#ffffff66] shadow-inner
+            shadow-[inset_0_2px_3px_rgba(255,255,255,0.3), inset_0_1px_0_rgba(0,0,0,0.4)]
+            flex items-center justify-between px-2 z-50">
+            <div className="flex items-center gap-2">
+
+            </div>
+        </div>
+    )
+}
+
+export default Taskbar;
