1f5a0ebd10cd04dea638591614cedfb43acccca9
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-06-22 18:41:24 -0700

    feat: footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+    import './Footer.css';
+</script>
+
+<footer class="footer">
+    <div class="footer-content">
+        <p>Copyright {new Date().getFullYear()} (c) Jeremiah Soe. Built with ❤️ using SvelteKit.</p>
+        <div class="socials">
+            <a href="https://github.com/fiveminus1" target="_blank">GitHub</a>
+            <a href="https://www.linkedin.com/in/jeremiah-soe/" target="_blank">LinkedIn</a>
+            <a href="mailto:jeremiahjsoe@gmail.com">Email</a>
+        </div>
+    </div>
+</footer>
