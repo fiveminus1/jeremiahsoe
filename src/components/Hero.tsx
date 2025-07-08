cb1e3e3fa7540886deb3deebb63ddfcff0eaac49
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-07-08 14:49:17 -0700

    feat: hero highlight

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,17 @@
-import { Box, Heading, Stack, Text } from "@chakra-ui/react";
+import { Box, Heading, Stack, Text, Highlight } from "@chakra-ui/react";
 
 const Hero = () => {
   return (
-    <Box id="hero" minH="100vh" display="flex" justifyContent="left" px={4}>
+    <Box id="hero" minH="100vh" display="flex" justifyContent="left" px={4} py={6}>
       <Stack direction="column" textAlign="left">
-        <Heading size="6xl" fontWeight="bold">Jeremiah Soe</Heading>
+        <Heading size="6xl" fontWeight="bold">
+          <Highlight 
+            query={["Jeremiah Soe"]}
+            styles={{ bg: "accentDark"}}
+          >
+          Blah blah, Jeremiah Soe.
+          </Highlight>
+        </Heading>
         <Text fontSize="lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
       </Stack>
     </Box>
