a6fa27bb044bcb268bbc5c2d3091eb21c0006665
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-07-08 15:04:42 -0700

    feat: more hero changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,9 @@
-import { Box, Heading, Stack, Text, Highlight } from "@chakra-ui/react";
+import { Box, Heading, Stack, Text, Highlight, HStack, IconButton } from "@chakra-ui/react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <Box id="hero" minH="100vh" display="flex" justifyContent="left" px={4} py={6}>
+    <Box id="hero" minH="100vh" display="flex" justifyContent="left" px={12} py={16}>
       <Stack direction="column" textAlign="left">
         <Heading size="6xl" fontWeight="bold">
           <Highlight 
@@ -12,7 +13,24 @@ const Hero = () => {
           Blah blah, Jeremiah Soe.
           </Highlight>
         </Heading>
-        <Text fontSize="lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+        <Text fontSize="lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+
+        <HStack py={4}>
+          <IconButton 
+            aria-label="GitHub"
+            variant="surface"
+          >
+            <FaGithub />
+          </IconButton>
+          <IconButton 
+            aria-label="LinkedIn"
+            variant="surface"
+          >
+            <FaLinkedin />
+          </IconButton>
+        </HStack>
       </Stack>
     </Box>
   );
