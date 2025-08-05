6107ad282156494458412683ca6a05078f8385d1
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-08-05 02:02:13 -0700

    refactor(ci): remove static export settings for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,17 @@
 import createMDX from '@next/mdx'
 
-const isProd = process.env.NODE_ENV === 'production';
-
-/** @type (import('next').NextConfig) */
-const nextConfig = {
-  output: "export",
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  basePath: isProd ? `/jeremiahsoe` : '',
-  assetPrefix: isProd ? `/jeremiahsoe` : '',
-  images: {
-    unoptimized: true,
-  },
-  transpilePackages: ['@next/mdx'],
-};
-
 const withMDX = createMDX({
   extension: /\.(md|mdx)$/,
 });
+
+/** @type (import('next').NextConfig) */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  images: {
+    unoptimized: true,
+  },
+};
+
+
 
 export default withMDX(nextConfig);
